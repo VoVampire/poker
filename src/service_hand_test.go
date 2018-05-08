a29f175bcd6aed6b3e0a212cf49576b76e3aebc7
zhangkai <zhangkai@qiyuanfin.com>
2018-05-08 11:21:15 +0800

    重构单元测试

--- src/service_hand_test.go
+++ src/service_hand_test.go
@@ -4,72 +4,24 @@ import (
 	"testing"
 )
 
-func TestCompare(t *testing.T) {
+func TestGetMaxHands(t *testing.T) {
+	assertTemplet(t, "XnJc5dKh3d4d6d", StraightFlush, 62)
+	assertTemplet(t, "JdJcJsJh3d4d6d", FourOfAKind, 281509340643856)
+	assertTemplet(t, "6dXnAc3c7dAh7s", FullHouse, 274911727648)
+	assertTemplet(t, "JdJc5dKh3d4d6d", Flush, 542)
+	assertTemplet(t, "9cXnKs2c6dTc7c", Flush, 4513)
+	assertTemplet(t, "JdJcKcKh3d4d6d", TwoPair, 20974096)
+	assertTemplet(t, "Qs9dAh6hAc6d9c", TwoPair, 34608256)
+	assertTemplet(t, "Ad3hQhJc2c8s2s", OnePair, 13825)
+	assertTemplet(t, "JdJc5hKh3d4d6d", OnePair, 4196888)
+	assertTemplet(t, "Jd5d9cXnKs2c6d", OnePair, 16779920)
+}
 
-	if maxHand := analyzeHandStr("Qs9dAh6hAc6d9c").getMaxHands(); maxHand.MaxCase != TwoPair {
-		t.Errorf("PokerType 错误，应该为 %d，结果为 %d", TwoPair, maxHand.MaxCase)
-	} else if maxHand.MaxHand != 34608256 {
-		t.Errorf("Value 错误，应该为\n%b，结果为 \n%b", 34608256, maxHand.MaxHand)
-	}
-
-	if maxHand := analyzeHandStr("Jd5d9cXnKs2c6d").getMaxHands(); maxHand.MaxCase != OnePair {
-		t.Errorf("PokerType 错误，应该为 %d，结果为 %d", OnePair, maxHand.MaxCase)
-	} else if maxHand.MaxHand != 16779920 {
-		t.Errorf("Value 错误，应该为\n%b，结果为 \n%b", 16779920, maxHand.MaxHand)
-	}
-
-	if maxHand := analyzeHandStr("9cXnKs2c6dTc7c").getMaxHands(); maxHand.MaxCase != Flush {
-		t.Errorf("PokerType 错误，应该为 %d，结果为 %d", Flush, maxHand.MaxCase)
-	} else if maxHand.MaxHand != 4513 {
-		t.Errorf("Value 错误，应该为\n%b，结果为 \n%b", 4513, maxHand.MaxHand)
-	}
-
-	if maxHand := analyzeHandStr("Ad3hQhJc2c8s2s").getMaxHands(); maxHand.MaxCase != OnePair { // 一对 11011000000001
-		t.Errorf("PokerType 错误，应该为 %d，结果为 %d", OnePair, maxHand.MaxCase)
-	} else if maxHand.MaxHand != 13825 {
-		t.Errorf("Value 错误，应该为\n%b，结果为 \n%b", 13825, maxHand.MaxHand)
-	}
-
-	// 葫蘆 100000000000010000001000001000000100000
-	if maxHand := analyzeHandStr("6dXnAc3c7dAh7s").getMaxHands(); maxHand.MaxCase != FullHouse {
-		t.Errorf("PokerType 错误，应该为 %d，结果为 %d", FullHouse, maxHand.MaxCase)
-	} else if maxHand.MaxHand != 274911727648 {
-		t.Errorf("Value 错误，应该为\n%b，结果为 \n%b", 274911727648, maxHand.MaxHand)
-	}
-
-	// 四條 0001000000000000100000000000010000000000001000010000
-	if maxHand := analyzeHandStr("JdJcJsJh3d4d6d").getMaxHands(); maxHand.MaxCase != FourOfAKind {
-		t.Errorf("PokerType 错误，应该为 %d，结果为 %d", FourOfAKind, maxHand.MaxCase)
-	} else if maxHand.MaxHand != 281509340643856 {
-		t.Errorf("Value 错误，应该为\n%b，结果为 \n%b", 281509340643856, maxHand.MaxHand)
-	}
-
-	// 一对
-	if maxHand := analyzeHandStr("JdJc5hKh3d4d6d").getMaxHands(); maxHand.MaxCase != OnePair {
-		t.Errorf("PokerType 错误，应该为 %d，结果为 %d", OnePair, maxHand.MaxCase)
-	} else if maxHand.MaxHand != 4196888 {
-		t.Errorf("Value 错误，应该为 %d，结果为 %d", 4196888, maxHand.MaxHand)
-	}
-
-	// 同花顺 111110
-	if maxHand := analyzeHandStr("XnJc5dKh3d4d6d").getMaxHands(); maxHand.MaxCase != StraightFlush {
-		t.Errorf("PokerType 错误，应该为 %d，结果为 %d", StraightFlush, maxHand.MaxCase)
-	} else if maxHand.MaxHand != 62 {
-		t.Errorf("Value 错误，应该为 %d，结果为 %d", 62, maxHand.MaxHand)
-	}
-
-	// 同花 1000011110
-	if maxHand := analyzeHandStr("JdJc5dKh3d4d6d").getMaxHands(); maxHand.MaxCase != Flush {
-		t.Errorf("PokerType 错误，应该为 %d，结果为 %d", Flush, maxHand.MaxCase)
-	} else if maxHand.MaxHand != 542 {
-		t.Errorf("Value 错误，应该为 %d，结果为 %d", 542, maxHand.MaxHand)
-	}
-
-	// 两对 1010000000000101000010000
-	if maxHand := analyzeHandStr("JdJcKcKh3d4d6d").getMaxHands(); maxHand.MaxCase != TwoPair {
-		t.Errorf("PokerType 错误，应该为 %d，结果为 %d", TwoPair, maxHand.MaxCase)
-	} else if maxHand.MaxHand != 20974096 {
-		t.Errorf("Value 错误，应该为 %d，结果为 %d", 20974096, maxHand.MaxHand)
+func assertTemplet(t *testing.T, handStr string, expectCase, expectHand uint64) {
+	if maxHand := analyzeHandStr(handStr).getMaxHands(); maxHand.MaxCase != expectCase {
+		t.Errorf("MaxCase 错误，应该为 %d，结果为 %d", expectCase, maxHand.MaxCase)
+	} else if maxHand.MaxHand != expectHand {
+		t.Errorf("MaxHand 错误，应该为 %d，结果为 %d", expectHand, maxHand.MaxHand)
 	}
 }
 
